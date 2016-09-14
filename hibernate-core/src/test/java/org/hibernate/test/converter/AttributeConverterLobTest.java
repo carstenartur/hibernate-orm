62d9a99b71b6dc4142f37b5ee921bf37acd8aba3
Carsten Hammer <carsten.hammer@t-online.de>
2016-09-14 07:56:54 +0200

    HHH-11098

--- hibernate-core/src/test/java/org/hibernate/test/converter/AttributeConverterLobTest.java
+++ hibernate-core/src/test/java/org/hibernate/test/converter/AttributeConverterLobTest.java
@@ -6,82 +6,122 @@
  */
 package org.hibernate.test.converter;
 
-import java.sql.Types;
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.AttributeConverter;
 import javax.persistence.Convert;
 import javax.persistence.Converter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.type.Type;
-import org.hibernate.type.descriptor.converter.AttributeConverterTypeAdapter;
-
-import org.hibernate.testing.junit4.BaseUnitTestCase;
-import org.junit.After;
-import org.junit.Before;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.Query;
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
 
-import static org.hibernate.testing.junit4.ExtraAssertions.assertTyping;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Test mapping a model with an attribute combining {@code @Lob} with an AttributeConverter.
- * <p/>
- * Originally developed to diagnose HHH-9615
+ * Test to check the number of attributeconverter calls on a simple save and list
  *
- * @author Steve Ebersole
+ * @author Carsten Hammer
  */
-public class AndLobTest extends BaseUnitTestCase {
-	private StandardServiceRegistry ssr;
-
-	@Before
-	public void before() {
-		ssr = new StandardServiceRegistryBuilder().build();
+public class AttributeConverterLobTest extends BaseCoreFunctionalTestCase {
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class[] { EntityImpl.class };
 	}
-
-	@After
-	public void after() {
-		if ( ssr != null ) {
-			StandardServiceRegistryBuilder.destroy( ssr );
-		}
+	
+	@Override
+	public void configure(Configuration cfg) {
+		super.configure( cfg );
+//		cfg.setProperty( Environment.USE_SECOND_LEVEL_CACHE, "true" );
+		cfg.setProperty( Environment.GENERATE_STATISTICS, "true" );
 	}
 
 	@Test
 	public void testMappingAttributeWithLobAndAttributeConverter() {
-		final Metadata metadata = new MetadataSources( ssr )
-				.addAnnotatedClass( EntityImpl.class )
-				.buildMetadata();
-
-		final Type type = metadata.getEntityBinding( EntityImpl.class.getName() ).getProperty( "status" ).getType();
-		final AttributeConverterTypeAdapter concreteType = assertTyping( AttributeConverterTypeAdapter.class, type );
-		assertEquals( Types.BLOB, concreteType.getSqlTypeDescriptor().getSqlType() );
+		Session session = openSession();
+		session.beginTransaction();
+		EntityImpl object = new EntityImpl();
+		object.status=new HashMap<>();
+		object.status.put( "asdf", Integer.valueOf( 6 ) );
+		object.status.put( "key", "table" );
+		object.id=1;
+		session.save( object );
+		session.getTransaction().commit();
+		session.close();
+		/**
+		 * What? Why the hell 2 and not 1?
+		 */
+		assertEquals(2,ConverterImpl.todatabasecounter);
+		/**
+		 * Why a from database conversion at all?
+		 */
+		assertEquals(1,ConverterImpl.fromdatabasecounter);
+		
+		session = openSession();
+		session.beginTransaction();
+		Query<EntityImpl> createQuery = session.createQuery( "select e from EntityImpl e", EntityImpl.class );
+		List<EntityImpl> resultList = createQuery.getResultList();
+		assertEquals(1,resultList.size());
+		session.getTransaction().commit();
+		session.close();
+		/**
+		 * Why again a to database conversion? These conversions are very expensive and should only be done if really needed..
+		 */
+		assertEquals(3,ConverterImpl.todatabasecounter);
+		assertEquals(3,ConverterImpl.fromdatabasecounter);
+		assertEquals("table",resultList.get(0 ).status.get( "key" ));
+		assertEquals(3,ConverterImpl.fromdatabasecounter);
 	}
 
 	@Converter
-	public static class ConverterImpl implements AttributeConverter<String, Integer> {
+	public static class ConverterImpl implements AttributeConverter<Map, byte[]> {
+		public static int todatabasecounter=0;
+		public static int fromdatabasecounter=0;
 		@Override
-		public Integer convertToDatabaseColumn(String attribute) {
-			return attribute.length();
+		public byte[] convertToDatabaseColumn(Map map) {
+			todatabasecounter++;
+			ByteArrayOutputStream out=new ByteArrayOutputStream();
+			try(XMLEncoder encoder=new XMLEncoder(out)){
+				encoder.writeObject( map );
+			}
+			return out.toByteArray();
 		}
 
 		@Override
-		public String convertToEntityAttribute(Integer dbData) {
-			return "";
+		public Map convertToEntityAttribute(byte[] dbData) {
+			fromdatabasecounter++;
+			try(ByteArrayInputStream in=new ByteArrayInputStream(dbData)){
+				XMLDecoder decoder=new XMLDecoder(in);
+				return (Map) decoder.readObject();
+			}
+			catch (IOException e) {
+				return null;
+			}
 		}
 	}
 
-	@Entity
+	@Entity(name = "EntityImpl")
+	@Table( name = "EntityImpl" )
 	public static class EntityImpl {
 		@Id
 		private Integer id;
 
 		@Lob
 		@Convert(converter = ConverterImpl.class)
-		private String status;
+		private Map status;
 	}
 }
